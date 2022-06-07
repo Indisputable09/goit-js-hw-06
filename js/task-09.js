31e858eecbe92aa8b85ce7a2d16d086afe9ede24
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-06-07 16:50:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,7 +2,7 @@ const colorEl = document.querySelector('.color');
 const btnEl = document.querySelector('.change-color');
 const bodyEl = document.querySelector('body');
 
-bodyEl.style.backgroundColor = btnEl.addEventListener('click', getRandomHexColor);
+btnEl.addEventListener('click', getRandomHexColor);
 
 function getRandomHexColor() {
   const randomColor = `#${Math.floor(Math.random() * 16777215)
